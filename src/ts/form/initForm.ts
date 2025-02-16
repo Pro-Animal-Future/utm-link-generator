c7d686fd95cd904c8bd6ee949f5387e6b790c696
Eric Arellano <ericarellano@me.com>
2025-02-16 18:16:24 -0500

    Generate HTML for communication type

--- src/ts/form/initForm.ts
+++ src/ts/form/initForm.ts
@@ -1,15 +1,33 @@
 import { CommunicationType, FormState } from "../state/FormState";
 import Observable from "../state/Observable";
+import { generateRadioGroup } from "./radio";
 
-export function initForm(formState: Observable<FormState>): void {
-  const communicationTypeFieldSet = document.getElementById(
-    "communication-type",
-  ) as HTMLFieldSetElement;
-  communicationTypeFieldSet.addEventListener("change", (e) => {
+function initCommunicationType(
+  form: HTMLFormElement,
+  formState: Observable<FormState>,
+): void {
+  const fieldSet = generateRadioGroup({
+    id: "communication-type",
+    label: "Communication type",
+    options: [
+      { value: "ad", label: "Advertisement" },
+      { value: "email", label: "Email" },
+      { value: "field", label: "Field" },
+      { value: "social", label: "Organic social media" },
+    ],
+  });
+  form.appendChild(fieldSet);
+
+  fieldSet.addEventListener("change", (e) => {
     if (!(e.target instanceof HTMLInputElement)) return;
     formState.setValue({
       ...formState.getValue(),
       type: e.target.value as CommunicationType,
     });
   });
+}
+
+export function initForm(formState: Observable<FormState>): void {
+  const form = document.getElementById("utm-form") as HTMLFormElement;
+  initCommunicationType(form, formState);
 }
