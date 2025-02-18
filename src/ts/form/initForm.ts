bd6b6f9dd37fd392b5454ad9b53d324b57f2d4d7
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-02-17 20:58:05 -0500

    Design radio buttons (#9)
    Based on https://designsystem.digital.gov/components/radio-buttons/.

--- src/ts/form/initForm.ts
+++ src/ts/form/initForm.ts
@@ -52,13 +52,26 @@ function initAdOptions(
     id: "ad-medium",
     label: "Medium",
     options: [
-      { value: "paid_social", label: "Social media ads (Meta, Reddit, etc)" },
-      { value: "paid_search", label: "Search ads (Google, Bing, etc)" },
+      {
+        value: "paid_social",
+        label: "Social media ads",
+        description: "Meta, Reddit, etc.",
+      },
+      {
+        value: "paid_search",
+        label: "Search ads",
+        description: "Google, Bing, etc.",
+      },
       {
         value: "paid_ooh",
-        label: "Out-of-home ads (billboards, transit ads, bus shelters",
+        label: "Out-of-home ads",
+        description: "Billboards, transit ads, bus shelters",
       },
-      { value: "paid_sms", label: "Text blasts (Scale to Win)" },
+      {
+        value: "paid_sms",
+        label: "Text blasts",
+        description: "Aka Scale to Win",
+      },
       { value: "paid_mail", label: "Mass-mailed voter mailers" },
       { value: "paid_tv", label: "TV commercials" },
     ],
